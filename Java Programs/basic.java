718e6c325293a46450e4f8fa7c31c8ef706c7cf4
Raviranjan Kumar <raviranjankrmaurya@gmail.com>
2026-04-04 07:19:09 +0000

    first java program

---
+++ Java Programs/basic.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Arithmatic{
+    void display(){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter Your age:");
+    int age = sc.nextInt();
+    System.out.println("Your age is :" + age);
+
+    
+
+    sc.close();
+    }
+}
+
+class basic {
+
+    public static void main(String[] args) {
+        System.out.println("Raviranjan");
+        Arithmatic ss = new Arithmatic();
+        ss.display();
+
+    }
+}
